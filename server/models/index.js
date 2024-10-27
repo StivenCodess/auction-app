78e1c1ae676d171d8ee46e23a5baf280470f8081
Tiven <vairtor@gmail.com>
2024-10-26 21:33:24 -0300

    feat(server): set up Express server
    - Initialized and configured Express server

--- server/models/index.js
+++ server/models/index.js
@@ -1,4 +1,4 @@
 export * from "./Bid.js";
+export * from "./Product.js";
 export * from "./User.js";
 export * from "./Auction.js";
-export * from "./Product.js";
--- server/server.js
+++ server/server.js
@@ -1,8 +1,18 @@
+import express from "express";
 import { connectDB, sequelize } from "./config/index.js";
 import { User, Bid, Auction, Product } from "./models/index.js";
 
-async function main() {
-  await sequelize.sync();
-}
+import productRoutes from "./routes/product.routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(productRoutes);
+
+const main = async () => {
+  await sequelize.sync({ force: false, logging: false });
+  app.listen(5000);
+  console.log("Server ins running on port", 5000);
+};
 
 main();
