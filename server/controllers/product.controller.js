7ad91dd84701d344c83e8a5bfbda2cb9d37036a6
TivenNotebook <vairtor@gmail.com>
2024-10-27 10:48:38 -0300

    feat(controllers): complete Product controllers with CRUD operations
    - Implemented create, delete, update, and getAll methods for Product

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -1,19 +1,64 @@
+import { Product } from "../models/index.js";
+
 export const getProducts = async (req, res) => {
-  res.send("Getting Products");
+  try {
+   const products = await Product.findAll()
+   res.status(200).json(products)
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch products' });
+  }
 };
 
 export const getProductByIndex = async (req, res) => {
-  res.send("Getting Product id");
+  try {
+    const {id} = req.params
+    const product = await Product.findByPk(id)
+  
+    if(!product) return res.status(404).json({ error: 'Product not found' });
+    res.status(200).json(product);  
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch product' });
+  }
 };
 
 export const createProduct = async (req, res) => {
-  res.send("Creating Product");
+  try {
+    const {name, description, price, image_url} = req.body
+    const newProduct = await Product.create({name, description, price, image_url})
+    res.status(201).json(newProduct)
+  } catch (error) {
+    res.status(500).json({error: "Failed to create product"})
+  }
 };
 
 export const updateProduct = async (req, res) => {
-  res.send("Updating Product");
+  try {
+    const {id} = req.params
+    const {name, description, image_url, price} = req.body
+
+    const product = await Product.findByPk(id)
+    if(!product) return res.status(404).json({ error: 'Product not found' });
+
+    product.update({name, description, image_url, price})
+    res.status(200).json(product);  
+
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update product' });
+  }
+
 };
 
 export const deleteProduct = async (req, res) => {
-  res.send("Deleting Product");
+  try {
+    const {id} = req.params
+    const product = await Product.findByPk(id)
+
+    if(!product) return res.status(404).json({ error: 'Product not found' })
+    product.destroy()
+
+    res.status(200).send()
+  } catch (error) {
+    res.status(500).json({error: "Failed to deleted product"})
+  }
+
 };
