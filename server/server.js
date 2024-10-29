845cf776b69c63ff6385ae062a623d46167610da
TivenNotebook <vairtor@gmail.com>
2024-10-28 21:15:50 -0300

    docs: Added reminder comment in server.js to populate roles table when switching PCs

--- server/server.js
+++ server/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { connectDB, sequelize } from "./config/index.js";
 
-import { User, Bid, Auction, Product } from "./models/index.js";
-import { productRoutes, auctionRoutes, userRoutes, bidRoutes} from "./routes/index.js"
+import { User, Bid, Auction, Product, Role } from "./models/index.js";
+import {
+  productRoutes,
+  auctionRoutes,
+  userRoutes,
+  bidRoutes,
+} from "./routes/index.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -15,6 +20,15 @@ app.use("/api", bidRoutes);
 
 const main = async () => {
   await sequelize.sync({ force: false, logging: false });
+
+  /* ADD ROLES TO DEV */
+  /*
+  await Role.bulkCreate([
+    { name: "Admin" },
+    { name: "User" },
+    { name: "Manager" },
+  ]);
+  */
   app.listen(5000);
   console.log("Server ins running on port", 5000);
 };
