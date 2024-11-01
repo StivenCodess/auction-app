3fc633e290bf90307ab850530b828038b5f5f211
Tiven <vairtor@gmail.com>
2024-10-31 21:52:30 -0300

    feat(controller): update user.controller to encrypt password, add login and revalidateTokenController functions, and rename createUser to registerUser

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,10 +1,13 @@
+import bcrypt from "bcryptjs";
+import { validationResult } from "express-validator";
+
+import { generateToken, verifyToken } from "../helpers/jwt.js";
 import { User } from "../models/index.js";
-import { admin } from "../config/index.js";
 
 export const getUsers = async (req, res) => {
   try {
     const user = await User.findAll();
-    res.status(200).json(user);
+    res.status(200).json({ ok: true, user });
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch user" });
   }
@@ -16,43 +19,82 @@ export const getUserByIndex = async (req, res) => {
     const user = await User.findByPk(id);
 
     if (!user) return res.status(404).json({ error: "user not found" });
-    res.status(200).json(user);
+    res.status(200).json({ ok: true, user });
   } catch (error) {
     res.status(500).json({ error: "Failed to fetch user" });
   }
 };
 
-const firebaseCreateUser = async (email, password) => {
-  try {
-    const userRecord = await admin.auth().createUser({
-      email,
-      password,
-    });
-    return userRecord.uid;
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-export const createUser = async (req, res) => {
+export const registerUser = async (req, res) => {
   try {
     const { name, email, address, phone, role_id, password } = req.body;
-    const uid = await firebaseCreateUser(email, password);
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res.status(400).json({ ok: false, errors: errors.mapped() });
+
+    const salt = bcrypt.genSaltSync();
+    const encryptedPassword = bcrypt.hashSync(password, salt);
 
     const newuser = await User.create({
-      id: uid,
       name,
       email,
       address,
       phone,
       role_id,
-      password,
+      password: encryptedPassword,
     });
 
-    res.status(201).json(newuser);
+    const token = generateToken({ name, uid: newuser.uid });
+
+    res.status(201).json({
+      ok: true,
+      newuser,
+      token,
+    });
   } catch (error) {
+    console.log(error);
     res.status(500).json({ error: "Failed to create user" });
   }
+};
+
+export const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user)
+      return res.status(404).json({ error: "User not found in the database" });
+
+    const validPassword = bcrypt.compareSync(password, user.password);
+
+    if (!validPassword)
+      return res.status(400).json({ error: "Incorrect password" });
+
+    const token = generateToken({ name: user.name, uid: user.uid });
+
+    res.status(200).json({
+      ok: true,
+      uid: user.uid,
+      name: user.name,
+      role: user.role_id,
+      email: user.email,
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ error: "Failed Login" });
+  }
+};
+
+export const revalidateTokenController = async (req, res) => {
+  const { name, uid } = req;
+
+  const token = generateToken({ name, uid });
+
+  res.send({ ok: true, token });
 };
 
 export const updateUser = async (req, res) => {
@@ -64,7 +106,7 @@ export const updateUser = async (req, res) => {
     if (!user) return res.status(404).json({ error: "user not found" });
 
     user.update({ name, email, address, phone, role_id });
-    res.status(200).json(user);
+    res.status(200).json({ ok: true, user });
   } catch (error) {
     res.status(500).json({ error: "Failed to update user" });
   }
@@ -78,7 +120,7 @@ export const deleteUser = async (req, res) => {
     if (!user) return res.status(404).json({ error: "user not found" });
     user.destroy();
 
-    res.status(200).send();
+    res.status(200).send({ ok: true });
   } catch (error) {
     res.status(500).json({ error: "Failed to deleted user" });
   }
