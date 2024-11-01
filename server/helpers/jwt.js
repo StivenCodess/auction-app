7a681ed986b688396fb4f161d22aa52dde321a41
Tiven <vairtor@gmail.com>
2024-10-31 21:49:24 -0300

    chore(helpers): add folder for JWT token generation and verification functions

---
+++ server/helpers/jwt.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key";
+
+export const generateToken = (payload, options = {}) => {
+  return jwt.sign(payload, JWT_SECRET, { expiresIn: "7d", ...options });
+};
+
+export const verifyToken = (token) => {
+  try {
+    return jwt.verify(token, JWT_SECRET);
+  } catch (error) {
+    console.log(error);
+    throw new Error("Invalid or expired token");
+  }
+};
